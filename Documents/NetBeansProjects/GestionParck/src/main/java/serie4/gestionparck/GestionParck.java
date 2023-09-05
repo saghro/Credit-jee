52ee65235d14691ba1c6baf2ddebdc34a6aa5bc1
saghro <ayoubsaghro27@gmail.com>
2023-09-05 16:59:03 +0100

    first commit

---
+++ Documents/NetBeansProjects/GestionParck/src/main/java/serie4/gestionparck/GestionParck.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package serie4.gestionparck;
+
+/**
+ *
+ * @author PC
+ */
+public class GestionParck {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
