2118935759f33048506d74a8c4eb4aab2cee56d0
saghro <ayoubsaghro27@gmail.com>
2023-09-05 18:05:56 +0100

    class vehicule

---
+++ Documents/NetBeansProjects/GestionParck/src/main/java/serie4/gestionparck/Vehicule.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package serie4.gestionparck;
+
+/**
+ *
+ * @author PC
+ */
+public class Vehicule {
+    
+}
